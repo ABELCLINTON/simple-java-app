99bb2c684fb23a862fd45cb0df3b31ceb811c552
ABELCLINTON <abelclintonhot@gmail.com>
2025-10-08 10:24:54 +0100

    Create ecs_task.tf

---
+++ ecs_task.tf
@@ -0,0 +1,36 @@
+#####################
+# Variables
+#####################
+variable "aws_account_id" {}
+variable "aws_region" {}
+variable "ecr_repo" {}
+variable "image_tag" {
+  default = "latest"
+}
+
+#####################
+# ECS Task Definition
+#####################
+resource "aws_ecs_task_definition" "task" {
+  family                   = "fargate-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "app"
+      image = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repo}:${var.image_tag}"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
