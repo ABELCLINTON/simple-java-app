c9465c14913e49d7bb6373b846a847be3c1c2cb9
ABELCLINTON <abelclintonhot@gmail.com>
2025-10-22 14:31:59 +0100

    Update ecs_task.tf

--- ecs_task.tf
+++ ecs_task.tf
@@ -1,34 +1,25 @@
-#####################
-# Variables
-#####################
-variable "aws_account_id" {}
-variable "aws_region" {}
-variable "ecr_repo" {}
 variable "image_tag" {
-  default "latest"
+  type        = string
+  description = "Docker image tag"
+  default     = "latest"
 }
 
-#####################
-# ECS Task Definition
-#####################
-resource "aws_ecs_task_definition" "task" {
+resource "aws_ecs_task_definition" "app" {
   family                   = "fargate-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu                      = 256
+  memory                   = 512
 
   container_definitions = jsonencode([
     {
-      name  = "app"
-      image = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${var.ecr_repo}:${var.image_tag}"
+      name      = "simple-java-app"
+      image     = "${aws_ecr_repository.app.repository_url}:${var.image_tag}"
       essential = true
       portMappings = [
         {
           containerPort = 80
           hostPort      = 80
-          protocol      = "tcp"
         }
       ]
     }
