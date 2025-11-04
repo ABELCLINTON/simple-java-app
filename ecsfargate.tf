ffd44336602935158d61467d8bd69beb96238346
ABELCLINTON <abelclintonhot@gmail.com>
2025-11-04 09:06:47 +0100

    Update ecsfargate.tf

--- ecsfargate.tf
+++ ecsfargate.tf
@@ -71,6 +71,30 @@ resource "aws_security_group" "fargate_sg" {
 resource "aws_ecs_cluster" "fargate_cluster" {
   name = "fargate-cluster"
 }
+# ECS Task Execution Role
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach the managed ECS execution policy
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 # ALB
 resource "aws_lb" "alb" {
   name               = "fargate-alb"
