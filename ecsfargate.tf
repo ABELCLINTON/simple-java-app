59c312aa30f5ecf1c0dd7614d57a8f3ff01dafc5
ABELCLINTON <abelclintonhot@gmail.com>
2025-11-04 12:01:01 +0100

    Update ecsfargate.tf

--- ecsfargate.tf
+++ ecsfargate.tf
@@ -143,7 +143,7 @@ resource "aws_ecs_task_definition" "task1" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-
+  execution_role_arn        = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
