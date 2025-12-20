cabfc1decabd134ce52fb53d68cfd556a7500678
ABELCLINTON <abelclintonhot@gmail.com>
2025-12-20 14:19:07 +0100

    Update ecsfargate.tf

--- ecsfargate.tf
+++ ecsfargate.tf
@@ -121,7 +121,7 @@ resource "aws_lb_target_group" "tg" {
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    matcher             = "200"
+    matcher             = "200-399"
   }
 }
 
